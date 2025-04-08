569013dbb4bd92e7d8667427438f48420faeb2c5
MariaLuisaRatto <100279943+MariaLuisaRatto@users.noreply.github.com>
2025-04-08 17:23:16 +0200

    add default

--- R/catcheR_enrichment.R
+++ R/catcheR_enrichment.R
@@ -24,8 +24,8 @@ catcheR_enrichment <- function(
     folder, 
     file,
     meta,
-    timepoint = "NULL",
-    control_gene = "NULL",
+    timepoint = "PSC",
+    control_gene = "SCR",
     min_cells_cluster = 70,
     min_cells_shRNA = 40){
   
